f3b9e5dc7348da887c7f8dd505b9a7beaee3d3ee
chuducthang77 <chuducthang77@gmail.com>
2026-05-01 11:26:06 -0400

    sweep

--- submit_gap_sweep_array.sh
+++ submit_gap_sweep_array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=pg_gap_sweep
-#SBATCH --account=YOUR_ACCOUNT_HERE
-#SBATCH --time=12:00:00
+#SBATCH --account=def-szepesva:wq
+#SBATCH --time=1:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=16G
 #SBATCH --array=0-100
