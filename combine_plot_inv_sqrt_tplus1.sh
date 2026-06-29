b41c39fc7d0fd71d8f9d449ad6c4e7c109c20d30
chuducthang77 <chuducthang77@gmail.com>
2026-06-29 19:08:20 -0400

    Add new experiments

--- combine_plot_inv_sqrt_tplus1.sh
+++ combine_plot_inv_sqrt_tplus1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=inv_sqrt_plot
-#SBATCH --account=YOUR_ACCOUNT_HERE
-#SBATCH --time=02:00:00
+#SBATCH --account=def-szepesva
+#SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=8G
 #SBATCH --output=logs/%x_%j.out
@@ -11,4 +11,4 @@ set -euo pipefail
 mkdir -p logs inv_sqrt_tplus1_results
 module load python scipy-stack || true
 
-python run_inv_sqrt_tplus1_plots.py combine-plot   --outdir inv_sqrt_tplus1_results   --num-arms 40   --num-arms-list 40,10   --horizon 10000000   --regret-scale 1000000.0   --c-values 0.5,1,2,4   --make-sample-path   --sample-paths 40   --sample-checkpoints 250   --method approx   --max-mean-change 0.08   --max-noise-change 0.35   --max-block-size 10000000   --block-quantile 0.995
+python run_inv_sqrt_tplus1_plots.py combine-plot   --outdir inv_sqrt_tplus1_results   --num-arms 1000   --num-arms-list 1000,10   --horizon 10000000   --regret-scale 1000000.0   --c-values 0.5,1,2,4   --make-sample-path   --sample-paths 40   --sample-checkpoints 250   --method approx   --max-mean-change 0.08   --max-noise-change 0.35   --max-block-size 10000000   --block-quantile 0.995
